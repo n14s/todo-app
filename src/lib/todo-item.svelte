7a2a860cb8fa00058d857724440a51c5083b8730
nik <niklas.mertens@ipa.fraunhofer.de>
2022-03-31 03:14:38 +0000

    develop html structure

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,11 @@
+<form action="" method="">
+    <input type="hidden" name="done" value="" />
+    <button aria-label="Mark done/not done">Done/NotDone</button>
+</form>
+<form action="" method="">
+    <input type="text"/>
+    <button aria-label="Save todo">Save todo</button>
+</form>
+<form action="" method="">
+    <button aria-label="Delete todo">Delete todo</button>
+</form>
