271f98af017bfb4c5f5e4dee5990fd5b52daa0c6
n14s <n14x@posteo.de>
2022-05-21 11:48:03 +0200

    (back) allow external request origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.v1.endpoints.todo import router as todoRouter
 
+
+
 app = FastAPI()
+
+origins = ["http://localhost"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(todoRouter, tags=["todo"], prefix="/todo")
 
