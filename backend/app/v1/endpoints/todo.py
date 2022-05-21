61b25fdb3b092caac3589ffd324b3660e3fdc4a8
n14s <n14x@posteo.de>
2022-05-21 11:47:29 +0200

    (back) add fastapi post

--- backend/app/v1/endpoints/todo.py
+++ backend/app/v1/endpoints/todo.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Body
 from fastapi.encoders import jsonable_encoder
 #from app.core.models.database import getTodos
 from app.core.models.todo import Todo
+from app.core.schemas.todo import TodoIn
+
 
 
 todos = [Todo("aufräumen"), Todo("schlafen")]
@@ -11,3 +13,8 @@ router = APIRouter()
 @router.get("/", response_description="todos retrieved")
 async def getTodos():
     return jsonable_encoder(todos)
+
+@router.post("/", status_code=201, response_description="todo posted")
+async def postTodo(todo_in: TodoIn):
+    print(todo_in)
+
