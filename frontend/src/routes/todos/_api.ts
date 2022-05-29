d7320e4b9290550129383e1b18394791a59955e0
n14s <n14x@posteo.de>
2022-05-29 21:46:58 +0200

    (front) use _api.ts as bridge to backend

--- frontend/src/routes/todos/_api.ts
+++ frontend/src/routes/todos/_api.ts
@@ -7,68 +7,17 @@ export const api = async ( requestEvent : RequestEvent, data? : Record<string, u
     let body = {}
     let status = 500
     let request = requestEvent.request
+    let url = "http://localhost:8000/todo"
 
-    switch (request.method.toUpperCase()) {
-        case "GET":
-            status = 200;
-            body = await prisma.todo.findMany();
-            break;
-
-        case "POST":
-            body = await prisma.todo.create({
-                data: {
-                    created_at: data.created_at as Date,
-                    text: data.text as string,
-                    done: data.done as boolean,
-                }
-            })
-            status = 201;
-            break;
-
-        case "DELETE":
-            status = 200;
-            body = await prisma.todo.delete({
-                where: {
-                    uid: requestEvent.params.uid
-                }
-            })
-            break;
-
-        case "PATCH":
-            status = 200;
-            if (data.text === null ) {
-                body = await prisma.todo.update({
-                    where: {
-                        uid: requestEvent.params.uid
-                    },
-                    data: {
-                        done: data.done
-                    }
-                })
-            } else {
-                body = await prisma.todo.update({
-                    where: {
-                        uid: requestEvent.params.uid
-                    },
-                    data: {
-                        text: data.text
-                    }
-                })
-            }
-            break;
+    const fetchOptions = {
+        method : request.method,
+        headers : {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
     }
 
-    if (request.method.toUpperCase() !== "GET" && request.headers.get("accept") !== "application/json") {
-        return {
-            status : 303,
-            headers : {
-                location : "/"
-            }
-        }
-    }
-
-    return {
-        status,
-        body
-    }
+    const response = await fetch(url, fetchOptions)
+    return response
 }
