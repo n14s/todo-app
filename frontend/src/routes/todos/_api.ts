68f7e67bcfcd10a58bed0b5ae39617f194a42a0a
n14s <n14x@posteo.de>
2022-08-21 09:52:37 +0200

    (front) assign request body in bridge

--- frontend/src/routes/todos/_api.ts
+++ frontend/src/routes/todos/_api.ts
@@ -1,13 +1,27 @@
 import type { RequestEvent } from "@sveltejs/kit/types/private";
-import PrismaClient from "$lib/prisma"
+import PrismaClient from "$lib/prisma";
 
 const prisma = new PrismaClient();
 
-export const api = async ( requestEvent : RequestEvent, data? : Record<string, unknown>) => {
-    let body = {}
-    let status = 500
-    let request = requestEvent.request
-    let url = "http://localhost:8000/todo"
+export const api = async (requestEvent: RequestEvent, data?: Record<string, unknown>) => {
+
+    let body = {};
+    let bodystring = "";
+    let status = 500;
+    let request = requestEvent.request;
+    let url = "http://localhost:8000/todo";
+
+    switch (request.method.toUpperCase()){
+            case "GET":
+                bodystring = JSON.stringify(data)
+                break
+            case "POST":
+                bodystring = JSON.stringify(data)
+                break
+            case "DELETE":
+                bodystring = JSON.stringify({"uid": requestEvent.params.uid})
+                break
+        }
 
     const fetchOptions = {
         method : request.method,
@@ -15,9 +29,10 @@ export const api = async ( requestEvent : RequestEvent, data? : Record<string, u
             "Content-Type": "application/json",
             "Accept": "application/json"
         },
-        body: JSON.stringify(data)
+        body: bodystring
     }
 
-    const response = await fetch(url, fetchOptions)
-    return response
+  const response = await fetch(url, fetchOptions);
+  return response;
+
 }
